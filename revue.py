ef37ef0dc0c991da910a2cce958c9892fc0dd70a
Samir Serhal <sabouserhal@yahoo.ca>
2021-11-10 14:44:39 -0500

    revue python

--- revue.py
+++ revue.py
@@ -1,36 +1,56 @@
-#Revue de python pour le cour ICS4U
-#11-09-2021
-#Samir Serhal
+# revue des concepts de programmation
+# 2021-11-09
+# contexte - menu de choix
 
-#Définition des noms dans le système
-TITRE = "Menu Alimentaire"
-Fancy = "|"
+# CONSTANTES
+TITRE = "Menu alimentaire"
+BEAUTIFUL = " | "
 
-#Affichage de bienvenue et introduction au  menu
-print("Bienvenue à notre restaurant")
-print(Fancy,TITRE,Fancy)
+# Liste
+menu = ["pomme", "biscuit", "chocolat","pizza", "hamburger"]
+
+#Affichage du menu
+print("Bienvenu à notre restaurant gratuit!! Voici le menu.")
+print(BEAUTIFUL + TITRE + BEAUTIFUL)
+print()
+for i in range(0, len(menu)) :
+    print(f"{i} - {menu[i]}")
+
 print("")
-#Les choix alimentaires
-a = "pomme"
-b = "biscuit"
-c = "chocolat"
 
-menu = [a, b, c]
-
-print("Il y a",len(menu),"choix dans le menu.")
-print("Les choix sont:")
-
-
-#for i in range (0, len(menu)):
-#   print("")
-#    print("{i} - {menu(i)}")
-
-print(a)
-print(b)
-print(c)
-
-print("Quelle est votre choix?")
-choix=input().lower()
-print("Vous avez choisi", choix)
+#Prise de décision
+code=True
+while code == True:
+    choix=input("Quelle est votre choix alimentaire? ").lower()
+    if choix == menu[0] or choix == "0":
+        print("Bien sur. Voici votre", menu[0],".")
+    elif choix == menu[1] or choix == "1":
+        print("Bien sur. Voici votre", menu[1], ".")
+    elif choix== menu[2] or choix == "2":
+        print("Bien sur. Voici votre", menu[2], ".")
+    elif choix == menu[3] or choix == "3":
+        print("Bien sure. Voici votre", menu[3], ".")
+    elif choix == menu[4] or choix == "4":
+        print("Bien sure. Voici votre", menu[4], ".") 
+    else:
+        print("Choix invalide") 
 
 
+#Démande l'utilisateur si ils sont satisfait
+    print("")
+    print("Êtes-vous satisfaits avec votre choix? oui ou non")
+    réponse=input().lower()
+   
+ 
+    if réponse == "oui":
+        print("Merci pour votre clientèle. À la prochaine.")
+        code=False
+        
+    elif réponse == "non":
+        print("S'il vous plaît choisisser encore.")
+        code=True
+    
+    else:
+        print("Choix invalide")
+
+# Fin du menu
