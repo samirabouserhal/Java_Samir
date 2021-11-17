42cc0763acb4be4228408765682c768bd4da8371
Samir Serhal <sabouserhal@yahoo.ca>
2021-11-17 10:35:43 -0500

    rearranging

---
+++ Java_Hard/FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg 
+{
+    public static void main (String [] agrs)
+    {
+        System.out.println("Je suis déterminé à apprendre comment coder.");
+        System.out.println("Aujourd'hui, nous sommes le 17 novembre 2021");
+    }
+}
